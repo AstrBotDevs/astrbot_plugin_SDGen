d83d51e246e5876e99fa7654000008a3bd08a60e
Maoer233 <1912584909@qq.com>
2025-06-25 09:07:41 +0800

    修改MML提示词

--- messages.py
+++ messages.py
@@ -12,7 +12,7 @@ def get_llm_prompt_prefix():
     return DEFAULT_LLM_PROMPT_PREFIX
 
 DEFAULT_LLM_PROMPT_PREFIX = (
-    "请根据以下描述生成用于 Stable Diffusion WebUI 的英文提示词，请返回一条逗号分隔的 `prompt` 英文字符串，适用于 Stable Diffusion web UI，其中 1、应包含主体、风格、光照、色彩等方面的描述，避免解释性文本，不需要 “prompt:” 等内容，不需要双引号包裹，如果没有对人物比例进行要求，默认是全身正比（避免chibi） 2、如果用词并不符合 Stable Diffusion WebUI 的常见词，请将其转换为常见prompt 3、如果用户有提供角色名，要以“角色名(作品名称)”的方式输入到绘图模型。例：“aris_(blue_archive)”，如果用户没提及，则不添加。4、若用户的关键词包含“补全”时，应当以用户提供的关键词来补全相关prompt（例如：如果用户的关键词有监狱，如果要你补全，那应该有镣铐）。5、直接返回 `prompt`，不要加任何额外说明。"
+    "请根据以下描述生成用于 Stable Diffusion WebUI 的英文提示词，请返回一条逗号分隔的 `prompt` 英文字符串，适用于 Stable Diffusion web UI，其中 1、应包含主体、风格、光照、色彩等方面的描述，避免解释性文本，不需要 “prompt:” 等内容，不需要双引号包裹（如果用户没有要求，避免添加chibi等Q版形象） 2、如果用词并不符合 Stable Diffusion WebUI 的常见词，请将其转换为常见prompt 3、如果用户有提供角色名，要以“角色名(作品名称)”的方式输入到绘图模型。例：“aris_(blue_archive)”，如果用户没提及，则不添加。4、若用户的关键词包含“补全”时，应当以用户提供的关键词来补全相关prompt（例如：如果用户的关键词有监狱，如果要你补全，那应该有镣铐）。5、直接返回 `prompt`，不要加任何额外说明。"
 )
 
 LLM_PROMPT_PREFIX = get_llm_prompt_prefix()
